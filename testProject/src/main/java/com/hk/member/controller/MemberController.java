7630752b3a3c11d1d047f876aad07f7b06f53f23
hk_edu <hk_edu@192.168.4.103>
2020-09-16 09:49:34 +0900

    컨트롤러,VO 정창원

---
+++ testProject/src/main/java/com/hk/member/controller/MemberController.java
@@ -0,0 +1,27 @@
+package com.hk.member.controller;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.hk.member.HomeController;
+
+@Controller
+public class MemberController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	@GetMapping("/member/list")
+	public String memberList(Locale locale, Model model) {
+		
+		logger.info("Welcome home! The client locale is {}.", locale);
+	
+		model.addAttribute("members", memberService.memberList());
+		
+		return "memberList";
+	}
+}
