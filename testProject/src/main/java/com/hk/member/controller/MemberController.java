87ae0b989c2fc1bd77c4af28e77bef8a2c5bd151
hk_edu <hk_edu@192.168.4.103>
2020-09-16 10:31:38 +0900

    정창원_컨트롤러

--- testProject/src/main/java/com/hk/member/controller/MemberController.java
+++ testProject/src/main/java/com/hk/member/controller/MemberController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.hk.member.service.MemberService;
+import com.hk.member.vo.MemberVO;
 
 @Controller
 public class MemberController {
@@ -27,5 +29,25 @@ public class MemberController {
 		model.addAttribute("members", memberService.memberList());
 		
 		return "memberList";
+	}
+	
+	@GetMapping("/member/register")
+	public String memberRegister(MemberVO member) {
+		return "memberRegister";
+	}
+	
+	@PostMapping("/member/register")
+	public String memberRegisterDone(Model model, MemberVO memberVO) {
+		int retVal = memberService.memberRegister(memberVO);
+		logger.info("insert 후에 성공인지 실패인지 알려줌.. ["+retVal +"]");
+		if (retVal < 1) { 
+			
+			model.addAttribute("error", "에러가 발생했습니다. 관리자에게 문의하세요");
+			return "memberRegisterError";
+		} else { 
+			model.addAttribute("mname", memberVO.getMname());
+			return "memberRegisterDone";
+		}
+				
 	}
 }
